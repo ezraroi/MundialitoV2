f8701afb218b491088830abe6bb8b838d1512c3c
roi.ezra <roi.ezra@appsflyer.com>
2024-06-07 10:29:56 +0300

    updating migrations

---
+++ Mundialito/Migrations/20240607071700_GameIntegrations.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mundialito.Migrations
+{
+    /// <inheritdoc />
+    public partial class GameIntegrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IntegrationsData",
+                table: "Games",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IntegrationsData",
+                table: "Games");
+        }
+    }
+}
