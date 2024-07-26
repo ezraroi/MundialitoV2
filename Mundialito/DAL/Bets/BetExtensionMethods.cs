4b94739c2516d9871c87c502fb850dae8c844432
roi.ezra <roi.ezra@appsflyer.com>
2024-07-26 17:18:36 +0300

    fied add new game to support game type

--- Mundialito/DAL/Bets/BetExtensionMethods.cs
+++ Mundialito/DAL/Bets/BetExtensionMethods.cs
@@ -1,37 +1,33 @@
 ﻿using Mundialito.DAL.Games;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace Mundialito.DAL.Bets;
 
 public static class BetExtensionMethods
 {
 
-    public static Boolean IsOpenForBetting(this Bet bet)
+    public static bool IsOpenForBetting(this Bet bet)
     {
         return bet.IsOpenForBetting(DateTime.UtcNow);
     }
 
-    public static Boolean IsOpenForBetting(this Bet bet, DateTime now)
+    public static bool IsOpenForBetting(this Bet bet, DateTime now)
     {
         return bet.Game.IsOpen(now);
     }
 
-    public static Boolean IsResolved(this Bet bet)
+    public static bool IsResolved(this Bet bet)
     {
         return bet.IsResolved(DateTime.UtcNow);
     }
 
-    public static Boolean IsResolved(this Bet bet, DateTime now)
+    public static bool IsResolved(this Bet bet, DateTime now)
     {
 
         return !bet.IsOpenForBetting(now) && !bet.Game.IsPendingUpdate(now);
     }
 
     // TODO - Should remove this
-    public static String Mark(this Bet bet)
+    public static string Mark(this Bet bet)
     {
         if (bet.HomeScore == bet.AwayScore) return "X";
         return bet.HomeScore > bet.AwayScore ? "1" : "2";
